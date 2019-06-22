cb55ef79714a8faa4bea1876a1a8b61713407ba7
Robin <1105080+openscript@users.noreply.github.com>
2019-06-23 00:37:18 +0200

    Add build date to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,6 @@ interface FooterProps {
   className?: string
 }
 
-const Footer: React.FC<FooterProps> = ({ className }) => <footer className={className}>Footer</footer>
+const Footer: React.FC<FooterProps> = ({ className }) => <footer className={className}>{new Date().toISOString()}</footer>
 
 export default Footer
