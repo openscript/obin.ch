50046e490fa60acb58865dc9ca1e4384e6b905b3
Robin <1105080+openscript@users.noreply.github.com>
2019-07-12 23:57:44 +0200

    Add theme-color meta

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { InjectedIntlProps, injectIntl } from 'react-intl'
 import Helmet from 'react-helmet'
+import { colors } from '../styles/variables'
 
 interface HeadProps extends InjectedIntlProps {
   language: string
@@ -15,7 +16,8 @@ const Head: React.FC<HeadProps> = ({ intl, language, title, author }) => (
       meta={[
         { name: 'description', content: intl.formatMessage({ id: 'description' }) },
         { name: 'keywords', content: intl.formatMessage({ id: 'keywords' }) },
-        { name: 'author', content: author }
+        { name: 'author', content: author },
+        { name: 'theme-color', content: colors.orange }
       ]}
     >
       <html lang={language} />
