418338462e050ba99313a29d3cc27931b1b579c1
Robin <1105080+openscript@users.noreply.github.com>
2019-07-13 02:05:44 +0200

    Add gutter to main bottom

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import styled from '@emotion/styled'
-import { colors } from '../styles/variables'
+import { colors, dimensions } from '../styles/variables'
 
 const StyledMain = styled.main`
   display: flex;
   flex-direction: column;
   flex-grow: 1;
   background-color: ${colors.dark};
+  padding-bottom: ${dimensions.layout.gutter};
 `
 
 interface MainProps {
